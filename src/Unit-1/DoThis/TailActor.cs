4c17671caa0b4fb1376d6821def6f752bcb57996
psripirom <pichit.sripirom@amadeus.com>
2017-05-20 15:57:50 +0700

    Lesson 1.6: The Actor Lifecycle Overide PreStart TailActor  for initial FileObserver Overide PostStop TailActor for Cleanup OS handles

--- src/Unit-1/DoThis/TailActor.cs
+++ src/Unit-1/DoThis/TailActor.cs
@@ -55,15 +55,21 @@ namespace WinTail
 
         private readonly string _filePath;
         private readonly IActorRef _reporterActor;
-        private readonly FileObserver _observer;
-        private readonly Stream _fileStream;
-        private readonly StreamReader _fileStreamReader;
+        private FileObserver _observer;
+        private Stream _fileStream;
+        private StreamReader _fileStreamReader;
 
         public TailActor(IActorRef reporterActor, string filePath)
         {
             _reporterActor = reporterActor;
             _filePath = filePath;
+        }
 
+        /// <summary>
+        /// Initialization logic for actor that will tail changes to a file
+        /// </summary>
+        protected override void PreStart()
+        {
             // start watching file for changes
             _observer = new FileObserver(Self, Path.GetFullPath(_filePath));
             _observer.Start();
@@ -104,6 +110,17 @@ namespace WinTail
             }
         }
 
-    
+        /// <summary>
+        /// Cleanup OS handles for <see cref="_fileStreamReader"/>
+        /// </summary>
+        protected override void PostStop()
+        {
+            _observer.Dispose();
+            _observer = null;
+            _fileStreamReader.Close();
+            _fileStreamReader.Dispose();
+            base.PostStop();
+        }
+
     }
 }
